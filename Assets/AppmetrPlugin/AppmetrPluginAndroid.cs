e603f59bb1e637f26b5e4abd678faae4ad78df9b
Igor Durapov <igor.durapov@gmail.com>
2013-09-04 12:19:48 +0300

    Добавлен serializer для опций android плагине

--- Assets/AppmetrPlugin/AppmetrPluginAndroid.cs
+++ Assets/AppmetrPlugin/AppmetrPluginAndroid.cs
@@ -1,27 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
+using System.Web.Script.Serialization;
 
 public class AppmetrPluginAndroid : MonoBehaviour
 {
 	private static AndroidJavaObject currentActivity;
 	
-	private static AndroidJavaClass tapjoyConnect;
-	private static AndroidJavaClass TapjoyConnect
+	private static AndroidJavaClass clsConnect;
+	private static AndroidJavaClass Connect
 	{
 		get
 		{
-			if (tapjoyConnect == null)
+			if (clsConnect == null)
 			{
 				Debug.Log("C#: Loading AppmetrPlugin");
 			
 				AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
 				currentActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
 				
-				tapjoyConnect = new AndroidJavaClass("com.appmetr.android.AppMetr");
+				clsConnect = new AndroidJavaClass("com.appmetr.android.AppMetr");
 			}
 			
-			return tapjoyConnect;
+			return clsConnect;
 		}
 	}
 	
@@ -35,73 +36,92 @@ public class AppmetrPluginAndroid : MonoBehaviour
 
 	public static void AttachProperties(string properties)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(properties);
+		Connect.CallStatic("attachProperties", json);
 	}
 
 	public static void TrackSession()
 	{
-		TapjoyConnect.CallStatic("trackSession");
+		Connect.CallStatic("trackSession");
 	}
 
 	public static void TrackSession(string properties)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(properties);
+		Connect.CallStatic("trackSession", json);
 	}
 
 	public static void TrackLevel(int level)
 	{
-		TapjoyConnect.CallStatic("trackLevel", level);
+		Connect.CallStatic("trackLevel", level);
 	}
 
 	public static void TrackLevel(int level, string properties)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(properties);
+		Connect.CallStatic("trackLevel", level, json);
 	}
 
 	public static void TrackEvent(string _event)
 	{
-		TapjoyConnect.CallStatic("trackEvent", _event);
+		Connect.CallStatic("trackEvent", _event);
 	}
 
 	public static void TrackEvent(string _event, string properties)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(properties);
+		Connect.CallStatic("trackEvent", _event, json);
 	}
 
 	public static void TrackPayment(string payment)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(payment);
+		Connect.CallStatic("trackPayment", json);
 	}
 
 	public static void TrackPayment(string payment, string properties)
 	{
+		object jsonPayment = new JavaScriptSerializer.Deserialize<object>(payment);
+		object jsonProperties = new JavaScriptSerializer.Deserialize<object>(properties);
+		Connect.CallStatic("trackPayment", jsonPayment, jsonProperties);
 	}
 
 	public static void TrackGameState(string state, string properties)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(properties);
+		Connect.CallStatic("trackGameState", state, json);
 	}
 
 	public static void TrackOptions(string options, string commandId)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(options);
+		Connect.CallStatic("trackOptions", commandId, json);
 	}
 
 	public static void TrackOptions(string options, string commandId, string code, string message)
 	{
+		object json = new JavaScriptSerializer.Deserialize<object>(options);
+		Connect.CallStatic("trackOptionsError", commandId, json, code, message);
 	}
 
 	public static void TrackExperimentStart(string experiment, string group)
 	{
-		TapjoyConnect.CallStatic("trackExperimentStart", experiment, group);
+		Connect.CallStatic("trackExperimentStart", experiment, group);
 	}
 
 	public static void TrackExperimentEnd(string experiment)
 	{
-		TapjoyConnect.CallStatic("trackExperimentEnd", experiment);
+		Connect.CallStatic("trackExperimentEnd", experiment);
 	}
 
 	public static void PullCommands()
 	{
-		TapjoyConnect.CallStatic("pullCommands");
+		Connect.CallStatic("pullCommands");
 	}
 
 	public static void Flush()
 	{
-		TapjoyConnect.CallStatic("flush");
+		Connect.CallStatic("flush");
 	}
 
 	public static void SetDebugLoggingEnabled(bool debugLoggingEnabled)
