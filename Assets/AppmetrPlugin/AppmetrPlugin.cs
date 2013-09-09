bd4d35990ef28efdca307dcfac1c30b83564198f
Igor Durapov <igor.durapov@gmail.com>
2013-09-09 13:24:51 +0300

    Обновлен AppmetrPlugin

--- Assets/AppmetrPlugin/AppmetrPlugin.cs
+++ Assets/AppmetrPlugin/AppmetrPlugin.cs
@@ -11,4 +11,93 @@ using AppmetrPlatformPlugin = AppmetrPluginDefault;
 
 public class AppmetrPlugin : AppmetrWrapper
 {
+	public static void SetupWithToken(string token)
+	{
+		AppmetrPlatformPlugin.SetupWithToken(token);
+	}
+
+	public static void SetupWithUserID(string userID)
+	{
+		AppmetrPlatformPlugin.SetupWithUserID(userID);
+	}
+	
+	public static void AttachProperties(string properties)
+	{
+		AppmetrPlatformPlugin.AttachProperties(properties);
+	}
+	
+	public static void TrackSession()
+	{
+		AppmetrPlatformPlugin.TrackSession();
+	}
+	
+	public static void TrackSession(string properties)
+	{
+		AppmetrPlatformPlugin.TrackSession(properties);
+	}
+	
+	public static void TrackLevel(int level)
+	{
+		AppmetrPlatformPlugin.TrackLevel(level);
+	}
+	
+	public static void TrackLevel(int level, string properties)
+	{
+		AppmetrPlatformPlugin.TrackLevel(level, properties);
+	}
+	
+	public static void TrackEvent(string _event)
+	{
+		AppmetrPlatformPlugin.TrackEvent(_event);
+	}
+	
+	public static void TrackEvent(string _event, string properties)
+	{
+		AppmetrPlatformPlugin.TrackEvent(_event, properties);
+	}
+	
+	public static void TrackPayment(string payment)
+	{
+		AppmetrPlatformPlugin.TrackPayment(payment);
+	}
+	
+	public static void TrackPayment(string payment, string properties)
+	{
+		AppmetrPlatformPlugin.TrackPayment(payment, properties);
+	}
+	
+	public static void TrackGameState(string state, string properties)
+	{
+		AppmetrPlatformPlugin.TrackGameState(state, properties);
+	}
+	
+	public static void TrackOptions(string options, string commandId)
+	{
+		AppmetrPlatformPlugin.TrackOptions(options, commandId);
+	}
+	
+	public static void TrackOptions(string options, string commandId, string code, string message)
+	{
+		AppmetrPlatformPlugin.TrackOptions(options, commandId, code, message);
+	}
+	
+	public static void TrackExperimentStart(string experiment, string group)
+	{
+		AppmetrPlatformPlugin.TrackExperimentStart(experiment, group);
+	}
+	
+	public static void TrackExperimentEnd(string experiment)
+	{
+		AppmetrPlatformPlugin.TrackExperimentEnd(experiment);
+	}
+	
+	public static void PullCommands()
+	{
+		AppmetrPlatformPlugin.PullCommands();
+	}
+	
+	public static void Flush()
+	{
+		AppmetrPlatformPlugin.Flush();
+	}
 }
