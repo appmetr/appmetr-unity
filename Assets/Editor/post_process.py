0cdf61ba2715fc687150cec98f1327d352eb0c67
Igor Durapov <igor.durapov@gmail.com>
2013-09-16 15:21:00 +0300

    Небольшой фикс

--- Assets/Editor/post_process.py
+++ Assets/Editor/post_process.py
@@ -1,7 +1,8 @@
 import os
+import sys, os.path
 from sys import argv
 from mod_pbxproj import XcodeProject
-import appcontroller
+
 path = argv[1]
 
 project = XcodeProject.Load(path +'/Unity-iPhone.xcodeproj/project.pbxproj')
@@ -11,5 +12,27 @@ project.add_file('System/Library/Frameworks/StoreKit.framework', tree='SDKROOT')
 if project.modified:
     project.backup()
     project.saveFormat3_2()
+
 	
+install_path = sys.argv[1]
+target_platform = sys.argv[2]
+ 
+if target_platform != "iPhone": sys.exit()
+ 
+info_plist_path = os.path.join(install_path, 'Info.plist')
+ 
+file = open(info_plist_path, 'r')
+plist = file.read()
+file.close()
+ 
+elements_to_add = '''
+<key>appmetrUrl</key>
+<string>http://stage.pixapi.net/pixapi</string>
+'''
+ 
+plist = plist.replace('<key>', elements_to_add + '<key>', 1)
+ 
+file = open(info_plist_path, 'w')
+file.write(plist)
+file.close()
 	
