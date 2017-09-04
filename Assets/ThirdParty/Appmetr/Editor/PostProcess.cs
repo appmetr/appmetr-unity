7b18762c3da27370d94ef211b86fd754cb680aca
Andrey Gruzinov <a.gruzinov@pixonic.com>
2017-09-04 14:19:49 +0300

    B! Add defines into PostProcess

--- Assets/ThirdParty/Appmetr/Editor/PostProcess.cs
+++ Assets/ThirdParty/Appmetr/Editor/PostProcess.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿#if UNITY_IOS
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -40,3 +41,4 @@ namespace Appmetr.Unity.Editor
         }
     }
 }
+#endif
